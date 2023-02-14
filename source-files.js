559a52f3f83ef9385eed71f4f59fcd88c38bc02b
SeaDve <SeaDve@users.noreply.github.com>
2023-02-14 08:31:08 +0000

    deploy: 867a95141a3d2366313286bc406875273172d293

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"gsettings_macro":["",[["generators",[],["bitflag.rs","enumeration.rs","mod.rs","string.rs"]]],["imp.rs","lib.rs","schema.rs"]]\
+"gsettings_macro":["",[["generators",[],["bitflag.rs","enumeration.rs","mod.rs","string.rs"]]],["lib.rs","schema.rs"]]\
 }');
 createSourceSidebar();
