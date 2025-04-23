8e9a678b386b4d69fafd785b76578fa6445dfae3
SeaDve <SeaDve@users.noreply.github.com>
2025-04-23 03:24:45 +0000

    deploy: 085595be79130e3d9b95f2aa705cae96e80b0bff

--- crates.js
+++ crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["gsettings_macro"];
+//{"start":21,"fragment_lengths":[17]}
--- search-index.js
+++ search-index.js
@@ -1,5 +1,4 @@
-var searchIndex = new Map(JSON.parse('[\
-["gsettings_macro",{"t":"X","n":["gen_settings"],"q":[[0,"gsettings_macro"]],"i":[0],"f":"`","D":"d","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]\
-]'));
+var searchIndex = new Map(JSON.parse('[["gsettings_macro",{"t":"X","n":["gen_settings"],"q":[[0,"gsettings_macro"]],"i":"`","f":"`","D":"d","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA=","P":[]}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[168]}
