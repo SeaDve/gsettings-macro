38386cead1ed0d81cf5406512b6e7be86b65d797
SeaDve <SeaDve@users.noreply.github.com>
2025-08-25 00:32:05 +0000

    deploy: eddd9afcbe7496485d399e08da1d38563092b483

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["gsettings_macro",["",[["generators",[],["bitflag.rs","enumeration.rs","mod.rs","string.rs"]]],["lib.rs","schema.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[120]}
+createSrcSidebar('[["gsettings_macro",["",[["generators",[],["bitflag.rs","enumeration.rs","mod.rs","string.rs"]]],["lib.rs","schema.rs"]]]]');
+//{"start":19,"fragment_lengths":[120]}
